230ae813603256b04dd37c5220df4f042ae68346
ymiyauchi <ciero639@gmail.com>
2017-03-14 17:41:28 +0900

    gradle構成の構築

--- src/main/java/sample/DirectoryServer.java
+++ src/main/java/sample/DirectoryServer.java
@@ -1,4 +1,4 @@
-package main;
+package sample;
 
 import java.io.File;
 import java.io.IOException;
--- src/main/java/sample/Main.java
+++ src/main/java/sample/Main.java
@@ -1,4 +1,4 @@
-package main;
+package sample;
 
 import java.io.BufferedReader;
 import java.io.IOException;
