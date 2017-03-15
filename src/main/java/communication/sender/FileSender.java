545feacd940d51dbadef73b4c01ad54f3ac92a25
ymiyauchi <ciero639@gmail.com>
2017-03-15 10:06:36 +0900

    一時的に.ideaディレクトリをコミット

---
+++ src/main/java/communication/sender/FileSender.java
@@ -0,0 +1,22 @@
+package communication.sender;
+
+import java.io.File;
+import java.io.FileInputStream;
+import java.io.FileNotFoundException;
+import java.io.IOException;
+
+public class FileSender extends ExtendableSender {
+
+    public FileSender(Sender sender) {
+        super(sender);
+    }
+
+    public Sender put(File file) throws FileNotFoundException, IOException {
+        return put(new FileInputStream(file));
+    }
+
+    public Sender putFile(String filePath) throws IOException {
+        File file = new File(filePath);
+        return put(file);
+    }
+}
